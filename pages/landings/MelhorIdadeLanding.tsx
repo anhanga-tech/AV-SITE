04ef544ce56898eb4e71270fba4e85f76b1945c0
Felipe Williams <felipewilliam@gmail.com>
2026-03-12 16:40:48 -0300

    fix(ssr): import Link in MelhorIdadeLanding to fix prerender

--- pages/landings/MelhorIdadeLanding.tsx
+++ pages/landings/MelhorIdadeLanding.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { SEO } from '../../components/SEO';
 import { LandingFAQ } from '../../components/LandingFAQ';
 import { BreadcrumbSchema } from '../../components/schemas/BreadcrumbSchema';
